cc7cef1b6123b6649f739fa2a7d84abfb03dc77b
Hoang Cao Luu <hoangclw@gmail.com>
2024-06-01 21:50:28 +0700

    refactor: using maven instead of ant

---
+++ WS1/WS1-Mobile-CRUD/src/main/java/util/DBUtils.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author hd
+ */
+public class DBUtils {
+  public static final Connection getConnection() throws ClassNotFoundException, SQLException {
+    Connection conn = null;
+    Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=MobileCRUD", "sa", "12345");
+    return conn;
+  }
+
+  public static void main(String[] args) throws Exception {
+    try (Connection con = DBUtils.getConnection()) {
+      System.out.println("Connected successfully");
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+}
